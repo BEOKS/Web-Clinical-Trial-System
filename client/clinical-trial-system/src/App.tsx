78d9f9ee88fbf17927ad54955127925f20630822
doheez <doheedev@gmail.com>
2022-06-22 00:28:17 +0900

    Feat : create role selection page
    사용자 ROLE 선택 페이지를 생성한다.
    아직 가이드라인 수준이며 디자인 및 기능의 디테일 수정이 필요하다.

--- client/clinical-trial-system/src/App.tsx
+++ client/clinical-trial-system/src/App.tsx
@@ -1,10 +1,17 @@
 import Page from "./Page";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RoleSelection from "./component/RoleSelection/RoleSelection";
 
 function App() {
   return (
-    <div>
-      <Page />
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Page />}></Route>
+              <Route path="/select-role" element={<RoleSelection />}></Route>
+              {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는 경우 처리 */}
+              <Route path="*" element={<Page />}></Route>
+          </Routes>
+      </BrowserRouter>
   )
 }
 
--- client/clinical-trial-system/src/component/RoleSelection/RoleSelection.tsx
+++ client/clinical-trial-system/src/component/RoleSelection/RoleSelection.tsx
@@ -1,10 +1,45 @@
-import {Button} from "@mui/material";
-const RoleSelection=()=>{
+import {Button, Container, Paper, Stack, Typography} from "@mui/material";
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+
+const RoleSelection = () => {
     return (
-        <div>
-            <Button data-testid={'pi-button'}>PI</Button>
-            <Button data-testid={'reviewer-button'}></Button>
-        </div>
-    )
-}
+        <Container maxWidth="md">
+            <Paper variant="outlined" sx={{p: 5, my: 3}}>
+                <Stack spacing={2} sx={{display: 'flex', alignItems: 'center'}}>
+                    <Typography variant="h4">Select User Type</Typography>
+                    <Stack direction="row" spacing={4}>
+                        <Button data-testid={'pi-button'} variant="contained" sx={{width: '300px', p: 5}}>
+                            <Stack alignItems="center" spacing={3}>
+                                <AccountBoxIcon sx={{fontSize: 200}}/>
+                                <Typography>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has
+                                    been
+                                    the industry's standard dummy text ever since the 1500s
+                                </Typography>
+                                <Typography variant="h4">
+                                    PI
+                                </Typography>
+                            </Stack>
+                        </Button>
+                        <Button data-testid={'reviewer-button'} variant="contained" sx={{width: '300px', p: 5}}>
+                            <Stack alignItems="center" spacing={3}>
+                                <AccountBoxIcon sx={{fontSize: 200}}/>
+                                <Typography>
+                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                                    Ipsum has
+                                    been
+                                    the industry's standard dummy text ever since the 1500s
+                                </Typography>
+                                <Typography variant="h4">
+                                    Reviewer
+                                </Typography>
+                            </Stack>
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Paper>
+        </Container>
+    );
+};
 export default RoleSelection;
