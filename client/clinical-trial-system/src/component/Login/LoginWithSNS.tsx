6f4191fc324bd430b223c83830e8d378c6452aa4
doheez <doheedev@gmail.com>
2022-06-24 15:08:12 +0900

    Feat : add svg logo to sns login button
    SNS 계정 로그인 버튼 각각에 SVG 형식의 로고를 삽입한다.
    타입스크립트에서 svg 파일 import 에러가 있어, ReactComponent 대신 일반 img 태그를 사용한다.

--- client/clinical-trial-system/src/component/Login/LoginWithSNS.tsx
+++ client/clinical-trial-system/src/component/Login/LoginWithSNS.tsx
@@ -1,13 +1,9 @@
-import { Box, Button, Stack, Typography, styled, Divider } from "@mui/material";
-// import { ReactComponent as NaverLogo } from '../../assets/naverLogo.svg';
-// import { ReactComponent as KakaoLogo } from '../../assets/kakaoLogo.svg';
-// import { ReactComponent as GoogleLogo } from '../../assets/googleLogo.svg';
+import {Box, Button, Stack, Typography, styled, Divider} from "@mui/material";
+import NaverLogo from '../../assets/naverLogo.svg';
+import KakaoLogo from '../../assets/kakaoLogo.svg';
+import GoogleLogo from '../../assets/googleLogo.svg';
 
-// import NaverLogo from '../../assets/naverLogo.svg';
-// import KakaoLogo from '../../assets/naverLogo.svg';
-// import GoogleLogo from '../../assets/naverLogo.svg';
-
-const Root = styled('div')(({ theme }) => ({
+const Root = styled('div')(({theme}) => ({
     width: '100%',
     ...theme.typography.body2,
     '& > :not(style) + :not(style)': {
@@ -27,22 +23,19 @@ export default function LoginWithSNS() {
             </Root>
             <Stack spacing={1.5} mt={3}>
                 <Button variant="contained" size="large" color="naver">
-                    {/*<NaverLogo width="30px" />*/}
-                    {/*<img src={NaverLogo} alt='' width="30px"/>*/}
+                    <img src={NaverLogo} alt="naver" width="28px"/>
                     <Box width="100%">
                         Login with Naver
                     </Box>
                 </Button>
                 <Button variant="contained" size="large" color="kakao">
-                    {/*<KakaoLogo width="30px" />*/}
-                    {/*<img src={KakaoLogo} alt='' width="30px"/>*/}
+                    <img src={KakaoLogo} alt="kakao" width="30px"/>
                     <Box width="100%">
                         Login with Kakao
                     </Box>
                 </Button>
                 <Button variant="contained" size="large" color="google">
-                    {/*<GoogleLogo width="30px" />*/}
-                    {/*<img src={GoogleLogo} alt='' width="30px"/>*/}
+                    <img src={GoogleLogo} alt="google" width="30px"/>
                     <Box width="100%">
                         Login with Google
                     </Box>
