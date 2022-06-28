4bfe3caeeb7313f532d413e3db1dcd95db29934a
doheez <doheedev@gmail.com>
2022-06-29 07:29:41 +0900

    Feat : create add project dialog
    프로젝트 추가 버튼 클릭 시 나타날 프로젝트 추가 다이얼로그를 생성한다.

--- client/clinical-trial-system/src/component/AppBar/Project/AddProjectButton.tsx
+++ client/clinical-trial-system/src/component/AppBar/Project/AddProjectButton.tsx
@@ -1,16 +1,25 @@
 import AddIcon from '@mui/icons-material/Add';
 import {IconButton, Tooltip} from "@mui/material";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../../store";
+import {ProjectAction} from "./ProjectReducer";
 
 const AddProjectButton = () => {
+    const dispatch = useDispatch();
     const displaySelectProject = useSelector((state: RootState) => state.ProjectReducer.displaySelectProject);
+
+    const handleClickAddProject = () => {
+        dispatch(ProjectAction.openAddProjectDialog());
+    };
 
     return (
         <Tooltip title="Add Project">
             <IconButton aria-label="add project"
-                        sx={{ml: 2, border: 'solid 2px', borderColor: 'secondary.main', color: 'secondary.main',
-                            ...((!displaySelectProject) && {display: 'none'})}}>
+                        onClick={handleClickAddProject}
+                        sx={{
+                            ml: 2, border: 'solid 2px', borderColor: 'secondary.main', color: 'secondary.main',
+                            ...((!displaySelectProject) && {display: 'none'})
+                        }}>
                 <AddIcon/>
             </IconButton>
         </Tooltip>
