8530628e15ab96f12be1bd98724862328f9aaa4d
BEOKS <lee01042000@gmail.com>
2022-07-01 14:34:11 +0900

    Feat :ProjectTest list up
    ProjectController 에서 구현해야할 기능에
    대한 테스트코드 리스트를 작성하였습니다.

--- server/clinical-trial-system/src/test/java/com/example/clinicaltrialsystem/Project/ProjectControllerTest.java
+++ server/clinical-trial-system/src/test/java/com/example/clinicaltrialsystem/Project/ProjectControllerTest.java
@@ -1,12 +1,19 @@
 package com.example.clinicaltrialsystem.Project;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("Project")
 interface ProjectController{
     /**
      * 프로젝트 조회 기능
@@ -20,7 +27,6 @@ interface ProjectController{
      * @return Response : 정상적인 수행인 경우 위 정보를 리스트 형태로 제공합니다.
      *  에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
      */
-    @GetMapping("/")
     ResponseEntity<?> getProjectList();
 
     /**
@@ -30,7 +36,6 @@ interface ProjectController{
      * @param projectName : 새로 생성할 프로젝트 이름입니다.
      * @return 처리가 성공하면 200 메시지를 반환합니다. 에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
      */
-    @PostMapping("/{projectName}")
     ResponseEntity<?> createNewProject(@PathVariable String projectName);
 
     /**
@@ -40,7 +45,6 @@ interface ProjectController{
      * TODO 스키마는 차후 업데이트 예정입니다.
      * @return 처리가 성공하면 200 메시지를 반환합니다. 에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
      */
-    @PutMapping("/")
     ResponseEntity<?> updateProjectInformation();
 
     /**
@@ -52,11 +56,160 @@ interface ProjectController{
      *
      * @return 처리가 성공하면 200 메시지를 반환합니다. 에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
      */
-    @DeleteMapping("/")
     ResponseEntity<?> deleteProject();
 
 }
-@WebMvcTest
-public class ProjectControllerTest {
+@Controller
+@RequestMapping("Project")
+class BasicProjectController implements ProjectController{
+    @GetMapping("/")
+    @Override
+    public ResponseEntity<?> getProjectList() {
+        return null;
+    }
+    @PostMapping("/{projectName}")
+    @Override
+    public ResponseEntity<?> createNewProject( @PathVariable String projectName) {
+        return null;
+    }
+    @PutMapping("/")
+    @Override
+    public ResponseEntity<?> updateProjectInformation() {
+        return null;
+    }
+    @DeleteMapping("/")
+    @Override
+    public ResponseEntity<?> deleteProject() {
+        return null;
+    }
+}
+interface ProjectService{
 
+}
+@Service
+class ProjectServiceImpl implements ProjectService{
+
+}
+
+@WebMvcTest(ProjectController.class)
+@EnableAutoConfiguration
+public class ProjectControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ProjectService projectService;
+    @Test
+    @Tag("Request project information")
+    @DisplayName("PI")
+    public void getProjectListByPI(){
+
+    }
+    @Test
+    @Tag("Request project information")
+    @DisplayName("Reviewer")
+    public void getProjectListByReviewer(){
+
+    }
+    @Test
+    @Tag("Request project information")
+    @DisplayName("Unauthorized")
+    public void getProjectListByUnauthorized(){
+
+    }
+    @Test
+    @Tag("Request project information")
+    @DisplayName("No Role User")
+    public void getProjectListByNoRoleUser(){
+
+    }
+    @Test
+    @Tag("Request project information")
+    @DisplayName("project configuration")
+    public void getProjectConfigurationByPI(){
+
+    }
+    @Test
+    @Tag("Request project information")
+    @DisplayName("invalid project information request")
+    public void getProjectConfigurationByUnauthorizedOrElse(){
+
+    }
+    @Test
+    @Tag("Create new project")
+    @DisplayName("PI")
+    public void createNewProjectByPI(){
+
+    }
+    @Test
+    @Tag("Create new project")
+    @DisplayName("Unauthorized")
+    public void createNewProjectByUnauthorize(){
+
+    }
+    @Test
+    @Tag("Create new project")
+    @DisplayName("No Role or Reviewer")
+    public void createNewProjectByElse(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("Change Project Name")
+    public void changeProjectName(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("Migrate project manager(PI)")
+    public void migrateProjectManager(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("add project reviewer by PI")
+    public void appendNewReviewerInProject(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("add already in reviewer by PI")
+    public void appendAlreadyProjectInReviewer(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("add invalid user by PI")
+    public void appendInvalidUserOrReviewer(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("delete Reviewer in Project by PI")
+    public void deleteReviewerInProjectByPI(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("update project information by Unauthorized or Else")
+    public void updateProjectByUnauthorizedOrElse(){
+
+    }
+    @Test
+    @Tag("Update project information")
+    @DisplayName("delete Reviewer in Project by Unauthorized or Else")
+    public void deleteReviewerInProjectByUnauthorizedOrElse(){
+
+    }
+    @Test
+    @Tag("Delete project")
+    @DisplayName("delete project by PI")
+    public void deleteProjectByPI(){
+
+    }
+    @Test
+    @Tag("Delete project")
+    @DisplayName("delete project by unauthorized or else")
+    public void deleteProjectByUnauthorizedAndElse(){
+
+    }
 }
