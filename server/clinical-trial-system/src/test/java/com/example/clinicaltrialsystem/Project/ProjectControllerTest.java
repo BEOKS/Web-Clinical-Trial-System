27542b7e296fa9f9e71bb6426d3ce4ceb0a50ea5
BEOKS <lee01042000@gmail.com>
2022-07-05 08:37:38 +0900

    Fix : merge conflict  Conflicts: 	server/clinical-trial-system/src/main/java/com/example/clinicaltrialsystem/User/PI/PI.java 	server/clinical-trial-system/src/main/java/com/example/clinicaltrialsystem/User/User/UserProfile.java

--- server/clinical-trial-system/src/test/java/com/example/clinicaltrialsystem/Project/ProjectControllerTest.java
+++ server/clinical-trial-system/src/test/java/com/example/clinicaltrialsystem/Project/ProjectControllerTest.java
@@ -1,215 +1,216 @@
+/**
+ * Project Controller Test
+ * @Author : Jaeseong Lee, lee01042000@gmail.com
+ *
+ * @Reference
+ * 1. Testing OAuth 2.0, https://docs.spring.io/spring-security/reference/servlet/test/mockmvc/oauth2.html
+ * 2.
+ */
 package com.example.clinicaltrialsystem.Project;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-interface ProjectController{
-    /**
-     * 프로젝트 조회 기능
-     *
-     * 요청자의 역할에 따라 다른 프로젝트 조회 기능을 제공합니다.
-     * 1. Reviewer
-     * 현재 리뷰어가 참가하고 있는 프로젝트 리스트를 반환합니다.
-     * 2. PI
-     * 현재 PI가 관리하고 있는 프로젝트 리스트를 반환합니다.
-     *
-     * @return Response : 정상적인 수행인 경우 위 정보를 리스트 형태로 제공합니다.
-     *  에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
-     */
-    ResponseEntity<?> getProjectList();
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-    /**
-     * 프로젝트 생성 기능
-     * PI는 새로운 프로젝트를 생성 할 수 있습니다.
-     *
-     * @param projectName : 새로 생성할 프로젝트 이름입니다.
-     * @return 처리가 성공하면 200 메시지를 반환합니다. 에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
-     */
-    ResponseEntity<?> createNewProject(@PathVariable String projectName);
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-    /**
-     * 프로젝트 정보 업데이트 기능
-     *
-     * 사용자는 프로젝트 정보를 업데이트 할 수 있습니다.
-     * TODO 스키마는 차후 업데이트 예정입니다.
-     * @return 처리가 성공하면 200 메시지를 반환합니다. 에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
-     */
-    ResponseEntity<?> updateProjectInformation();
-
-    /**
-     * 프로젝트 삭제 기능
-     *
-     * PI가 프로젝트를 삭제 할 수 있습니다.
-     * 프로젝트 삭제 요청이 수행되면 프로젝트와 연관되어 있는 데이터도 모두 삭제됩니다.
-     * 삭제 전파는 데이터베이스에서 CASCADE 를 통해서 구현합니다.
-     *
-     * @return 처리가 성공하면 200 메시지를 반환합니다. 에러가 발생한 경우 에러 메시지가 포함된 응답이 전송됩니다.
-     */
-    ResponseEntity<?> deleteProject();
-
-}
-@Controller
-@RequestMapping("Project")
-class BasicProjectController implements ProjectController{
-    @GetMapping("/")
-    @Override
-    public ResponseEntity<?> getProjectList() {
-        return null;
-    }
-    @PostMapping("/{projectName}")
-    @Override
-    public ResponseEntity<?> createNewProject( @PathVariable String projectName) {
-        return null;
-    }
-    @PutMapping("/")
-    @Override
-    public ResponseEntity<?> updateProjectInformation() {
-        return null;
-    }
-    @DeleteMapping("/")
-    @Override
-    public ResponseEntity<?> deleteProject() {
-        return null;
-    }
-}
-interface ProjectService{
-
-}
-@Service
-class ProjectServiceImpl implements ProjectService{
-
-}
-
-@WebMvcTest(ProjectController.class)
-@EnableAutoConfiguration
+@WebMvcTest
 public class ProjectControllerTest {
-    @Autowired
+    public static final String USER_LOGIN_BUT_NO_SELECTED_ROLE = "User login but, no selected role";
+    public static final String UNAUTHORIZED_RESPONSE_MSG = "Unauthorized Request, Only Project Manager(PI) can request this api";
     private MockMvc mockMvc;
     @MockBean
     private ProjectService projectService;
-    @Test
-    @Tag("Request project information")
-    @DisplayName("PI")
-    public void getProjectListByPI(){
+
+
+    public List<Project> mockProjectList;
+    private String mockProjectListJsonString;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new BasicProjectController()).build();
+        mockProjectList = createMockListWith(() -> Project.createSampleProject());
+        mockProjectListJsonString = getMockProjectListJsonString();
 
     }
+
+    private List<Project> createMockListWith(Supplier<Project> createMockProject) {
+        return Stream.generate(createMockProject).limit(4).collect(Collectors.toList());
+    }
+
+    private String getMockProjectListJsonString() {
+        return mockProjectList.stream().map(Project::toJsonNode)
+                .collect(Collectors.toList()).toString();
+    }
+
+    @Test
+    @Tag("Request project list")
+    @DisplayName("PI")
+    @WithMockUser(roles = "PI")
+    public void getProjectListByPI() throws Exception {
+        Mockito.when(projectService.getProjectList()).thenReturn(mockProjectList);
+        mockMvc.perform(MockMvcRequestBuilders.get("/project"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string(mockProjectListJsonString));
+    }
+
     @Test
     @Tag("Request project information")
     @DisplayName("Reviewer")
-    public void getProjectListByReviewer(){
-
+    @WithMockUser(roles = "Reviewer")
+    public void getProjectListByReviewer() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/project"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string(UNAUTHORIZED_RESPONSE_MSG));
     }
+
     @Test
     @Tag("Request project information")
     @DisplayName("Unauthorized")
-    public void getProjectListByUnauthorized(){
-
+    @WithMockUser(roles = "null")
+    public void getProjectListByUnauthorized() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/project"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string(UNAUTHORIZED_RESPONSE_MSG));
     }
+
     @Test
     @Tag("Request project information")
     @DisplayName("No Role User")
-    public void getProjectListByNoRoleUser(){
-
+    public void getProjectListByNoRoleUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/project"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string(USER_LOGIN_BUT_NO_SELECTED_ROLE));
     }
+
     @Test
     @Tag("Request project information")
     @DisplayName("project configuration")
-    public void getProjectConfigurationByPI(){
-
+    @WithMockUser("PI")
+    public void getProjectConfigurationByPI() throws Exception {
+        Mockito.when(projectService.getProjectList()).thenReturn(mockProjectList);
+        mockMvc.perform(MockMvcRequestBuilders.get("/project"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string(mockProjectListJsonString));
     }
+
     @Test
     @Tag("Request project information")
     @DisplayName("invalid project information request")
-    public void getProjectConfigurationByUnauthorizedOrElse(){
-
+    public void getProjectConfigurationByUnauthorizedOrElse() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/project"))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string(UNAUTHORIZED_RESPONSE_MSG));
     }
+
     @Test
     @Tag("Create new project")
     @DisplayName("PI")
-    public void createNewProjectByPI(){
+    public void createNewProjectByPI() {
 
     }
+
     @Test
     @Tag("Create new project")
     @DisplayName("Unauthorized")
-    public void createNewProjectByUnauthorize(){
+    public void createNewProjectByUnauthorized() {
 
     }
+
     @Test
     @Tag("Create new project")
     @DisplayName("No Role or Reviewer")
-    public void createNewProjectByElse(){
+    public void createNewProjectByElse() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("Change Project Name")
-    public void changeProjectName(){
+    public void changeProjectName() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("Migrate project manager(PI)")
-    public void migrateProjectManager(){
+    public void migrateProjectManager() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("add project reviewer by PI")
-    public void appendNewReviewerInProject(){
+    public void appendNewReviewerInProject() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("add already in reviewer by PI")
-    public void appendAlreadyProjectInReviewer(){
+    public void appendAlreadyProjectInReviewer() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("add invalid user by PI")
-    public void appendInvalidUserOrReviewer(){
+    public void appendInvalidUserOrReviewer() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("delete Reviewer in Project by PI")
-    public void deleteReviewerInProjectByPI(){
+    public void deleteReviewerInProjectByPI() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("update project information by Unauthorized or Else")
-    public void updateProjectByUnauthorizedOrElse(){
+    public void updateProjectByUnauthorizedOrElse() {
 
     }
+
     @Test
     @Tag("Update project information")
     @DisplayName("delete Reviewer in Project by Unauthorized or Else")
-    public void deleteReviewerInProjectByUnauthorizedOrElse(){
+    public void deleteReviewerInProjectByUnauthorizedOrElse() {
 
     }
+
     @Test
     @Tag("Delete project")
     @DisplayName("delete project by PI")
-    public void deleteProjectByPI(){
+    public void deleteProjectByPI() {
 
     }
+
     @Test
     @Tag("Delete project")
     @DisplayName("delete project by unauthorized or else")
-    public void deleteProjectByUnauthorizedAndElse(){
+    public void deleteProjectByUnauthorizedAndElse() {
 
     }
 }
