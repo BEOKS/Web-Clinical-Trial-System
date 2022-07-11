bea5c06534b98a62a5180575839d30ce5a9ce2f0
BEOKS <lee01042000@gmail.com>
2022-07-11 15:19:37 +0900

    Fix : CreateReviewerDto.kt Description
    스웨거 예시 요청 형식이 잘못되던 버그가 수정되어
    이에 따라 설명도 수정하였습니다.

--- server/clinical-trial-system/src/main/java/com/beamworks/clinicaltrialsystem/User/Reviewer/Dto/CreateReviewerDto.kt
+++ server/clinical-trial-system/src/main/java/com/beamworks/clinicaltrialsystem/User/Reviewer/Dto/CreateReviewerDto.kt
@@ -6,7 +6,7 @@ import com.beamworks.clinicaltrialsystem.User.Reviewer.Reviewer
 data class CreateReviewerDto(val isSpeciality : Boolean,
                              val experienceYear: ExperienceYear) {
     companion object{
-        const val DESCRIPTION = "리뷰어 정보는 아래와 같은 JSON 형태로 전송 할 수 있습니다.(Swagger Example 은 버그입니다.)\n" +
+        const val DESCRIPTION = "리뷰어 정보는 아래와 같은 JSON 형태로 전송 할 수 있습니다.\n" +
                 "{\n" +
                 "\t\"experienceYear\": \"ZERO_TO_FIVE\"/\"MORE_THAN_FIVE\"\n" +
                 "\t\"isSpeciality\": true/false\n" +
