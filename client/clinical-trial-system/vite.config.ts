39973a049ce1d2230a527b0407410fb510a3edb0
BEOKS <lee01042000@gmail.com>
2022-07-15 21:35:17 +0900

    Fix : cross-env exception handling
    vite를 빌드하고 빌드 파일을 실행할 때
    cross-env로 설정한 환경변수가 제대로 입력되지 않는
    버그가 있어 임시로 prod 환경을 위한 코드를 작성하였습니다.

--- client/clinical-trial-system/vite.config.ts
+++ client/clinical-trial-system/vite.config.ts
@@ -1,6 +1,12 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-
+let HOST_LOCATION
+if(process.env.VITE_API_HOST){
+    HOST_LOCATION=process.env.VITE_API_HOST
+}
+else{
+    HOST_LOCATION='beamcts-env.eba-a5zfayqe.us-west-1.elasticbeanstalk.com'
+}
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
@@ -9,13 +15,16 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: `http://${process.env.API_HOST}`,
+                target: `http://${HOST_LOCATION}`,
                 changeOrigin: true
             },
             '/oauth2': {
-                target: `http://${process.env.API_HOST}`,
+                target: `http://${HOST_LOCATION}`,
                 changeOrigin: true
             },
         },
     },
+    define:{
+        'process.env':{}
+    }
 })
