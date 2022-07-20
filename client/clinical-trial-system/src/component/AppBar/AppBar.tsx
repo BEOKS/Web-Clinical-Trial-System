8871fd6709f4d8a70598132ca39d8749001cb027
doheez <doheedev@gmail.com>
2022-07-20 15:37:20 +0900

    Refactor : add comments to the app bar components

--- client/clinical-trial-system/src/component/AppBar/AppBar.tsx
+++ client/clinical-trial-system/src/component/AppBar/AppBar.tsx
@@ -1,3 +1,8 @@
+/**
+ * @file AppBar 관련 코드를 작성합니다.
+ * @author 김도희 <doheedev@gmail.com>
+ */
+
 import {AppBarProps as MuiAppBarProps} from "@mui/material/AppBar/AppBar";
 import {styled} from "@mui/material/styles";
 import MuiAppBar from "@mui/material/AppBar";
@@ -18,6 +23,12 @@ interface AppBarProps extends MuiAppBarProps {
     open?: boolean
 }
 
+/**
+ * MUI AppBar 컴포넌트에 트랜지션을 적용한 styled 컴포넌트입니다.
+ * Drawer를 열면 Drawer의 너비만큼 AppBar의 너비가 줄어들고
+ * Drawer를 닫으면 AppBar의 너비가 원래대로 돌아옵니다.
+ * @see {@link https://mui.com/system/styled/}
+ */
 const StyledAppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
 })<AppBarProps>(({theme, open}) => ({
@@ -35,6 +46,52 @@ const StyledAppBar = styled(MuiAppBar, {
     }),
 }));
 
+/**
+ * AppBar 좌측에 위치하는 메뉴 아이콘 버튼입니다.
+ * 버튼 클릭 시 Drawer를 열 수 있습니다.
+ * @param props.drawerOpen - 값이 true일 때 AppBar에서 메뉴 버튼이 보이지 않습니다.
+ * @param props.handleDrawerOpen - 메뉴 버튼 클릭 시 실행되는 함수입니다. [drawerOpen]{@link RootState.DrawerReducer.drawerOpen}을 true로 설정하여 Drawer를 엽니다.
+ * @param props.displayMenuButton - 값이 false일 때 AppBar에서 메뉴 버튼이 보이지 않습니다.
+ */
+const MenuButton = (props: { drawerOpen: boolean, handleDrawerOpen: () => void, displayMenuButton: boolean }) => {
+    return (
+        <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={props.handleDrawerOpen}
+            edge="start"
+            sx={{mr: 2, ...((props.drawerOpen || !props.displayMenuButton) && {display: 'none'})}}
+        >
+            <MenuIcon/>
+        </IconButton>
+    )
+}
+
+/**
+ * AppBar 좌측에 위치하는 리뷰 세션 제목입니다.
+ * @param props.displaySelectProject - 리뷰 세션 제목은 AppBar에 프로젝트 선택 뷰가 없을 때에만 보여야 하므로 [displaySelectProject]{@link RootState.ProjectReducer.displaySelectProject}가 false일 때 나타납니다.
+ * @todo displaySelectProject 값에 의존하면 로그인 페이지에서도 리뷰 세션 제목이 보이기 때문에 리뷰 관련 페이지에서만 보이도록 로직을 수정해야 합니다.
+ */
+const ReviewSessionTitle = (props: { displaySelectProject: boolean }) => {
+    return (
+        <Box>
+            {!props.displaySelectProject &&
+                <Typography variant="h6" component="div">
+                    CadAI-B<sup>TM</sup>
+                </Typography>
+            }
+        </Box>
+    )
+}
+
+/**
+ * 화면 최상단에 위치하는 AppBar 컴포넌트입니다.
+ * AppBar 자체와 우측에 배치한 회사 로고는 모든 페이지에서 항상 보여지며
+ * AppBar의 다른 구성요소인 메뉴 버튼, 프로젝트 선택 뷰, 프로젝트 생성 버튼, 리뷰 세션 제목은
+ * 페이지에 따라 display 여부를 변경할 수 있습니다.
+ * [displayMenuButton]{@link RootState.DrawerReducer.displayMenuButton}처럼
+ * 'display~'로 시작하는 state를 true로 설정하면 해당 컴포넌트가 보여집니다.
+ */
 const AppBar = () => {
     const dispatch = useDispatch();
     const drawerOpen = useSelector((state: RootState) => state.DrawerReducer.drawerOpen);
@@ -48,23 +105,12 @@ const AppBar = () => {
     return (
         <StyledAppBar position="fixed" open={drawerOpen}>
             <Toolbar>
-                <IconButton
-                    color="inherit"
-                    aria-label="open drawer"
-                    onClick={handleDrawerOpen}
-                    edge="start"
-                    sx={{mr: 2, ...((drawerOpen || !displayMenuButton) && {display: 'none'})}}
-                >
-                    <MenuIcon/>
-                </IconButton>
+                <MenuButton drawerOpen={drawerOpen} handleDrawerOpen={handleDrawerOpen}
+                            displayMenuButton={displayMenuButton}/>
                 <SelectProject/>
                 <AddProjectButton/>
                 <AddProjectDialog/>
-                {!displaySelectProject &&
-                    <Typography variant="h6" component="div">
-                        CadAI-B<sup>TM</sup>
-                    </Typography>
-                }
+                <ReviewSessionTitle displaySelectProject={displaySelectProject}/>
                 <Box flexGrow={2}/>
                 <Box>
                     <img src={beamworksLogoWhite} height={35} alt="beamworks logo"/>
